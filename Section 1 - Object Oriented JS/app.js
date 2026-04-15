6dff195aa070ecf19d1eea737720678f4cebb877
João Pedro Telles Fava <88334985+jaypeter19@users.noreply.github.com>
2026-04-15 00:40:13 -0300

    basic triangle class

--- Section 1 - Object Oriented JS/app.js
+++ Section 1 - Object Oriented JS/app.js
@@ -26,11 +26,19 @@ let myTri2 = {
 
 
 class Triangle {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
     getArea() {
         return (this.a + this.b) / 2;
     }
 
     getHypotenuse() {
         return Math.sqrt(this.a ** 2 + this.b ** 2);
+    }
+
+    sayHi(){
+        return 'SAYING HI FROM TRIANGLE'
     }
 }
